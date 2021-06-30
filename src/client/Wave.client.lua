fec085f8de959f9e689044913d05c8c7dc123244
Bilal-A-G <86686062+Bilal-A-G@users.noreply.github.com>
2021-06-30 09:39:22 -0400

    Added a system to start and stop wave movement
    Could be used for a chunk system

--- src/client/Wave.client.lua
+++ src/client/Wave.client.lua
@@ -1,35 +1,9 @@
 local ocean = game.Workspace:FindFirstChild("OceanPlane")
-local oceanBones = ocean:GetChildren()
-local runService = game:GetService("RunService")
-local waveSettings = require(game.ReplicatedStorage:WaitForChild("Common").WaveSettings)
-local clock = require(game.ReplicatedStorage:WaitForChild("Common").Clock)
-local players = game:GetService("Players")
-local player = players.LocalPlayer
-local character = player.Character
-if not character or character == nil then
-    character = player.CharacterAdded:Wait()
-end
+local ocean2 = game.Workspace:FindFirstChild("OceanPlane2")
+local waves = require(game.ReplicatedStorage:WaitForChild("Common").Waves)
 
-clock:Initialize()
-print("Initialized")
+waves.New("My First Wave", ocean)
+waves.New("My Second Wave", ocean2)
 
-local function SinesApproximation(point)
-    local wavelength = waveSettings.Calm_Wave.Wavelength
-    local amplitude = waveSettings.Calm_Wave.Amplitude
-    local speed = waveSettings.Calm_Wave.Speed
-    local windDirection = waveSettings.Calm_Wave.WindDirection
-
-    local w = 2/wavelength
-    local phaseConstant = speed * w
-
-    return amplitude * math.sin(windDirection:Dot(point) * w + clock:GetTime() * phaseConstant)
-end
-
-runService.Stepped:Connect(function()
-     for i,v in pairs(oceanBones) do
-        if v:IsA("Bone") then
-            local boneHeight = SinesApproximation(Vector2.new(v.Position.X, v.Position.Z))
-            v.Position = Vector3.new(v.Position.X, boneHeight, v.Position.Z)
-        end
-     end   
-end)
+waves:Update("My First Wave")
+waves:Update("My Second Wave")
---
+++ src/shared/Waves.lua
@@ -0,0 +1,82 @@
+local waveSettings = require(game.ReplicatedStorage:WaitForChild("Common").WaveSettings)
+local clock = require(game.ReplicatedStorage:WaitForChild("Common").Clock)
+local runService = game:GetService("RunService")
+local wave = {}
+
+wave.connections = {}
+
+clock:Initialize()
+
+function wave.New(waveName, wavePlane)
+    local newWave = {}
+    newWave.Name = waveName
+
+    local newConnection = {}
+    newConnection.Name = waveName
+    newConnection.connection = nil
+
+    newWave.bones = {}
+
+    for i,v in pairs(wavePlane:GetChildren()) do
+        if v:IsA("Bone") then
+            table.insert(newWave.bones, v)
+        end
+    end
+
+    table.insert(wave, newWave)
+    table.insert(wave.connections, newConnection)
+end
+
+function wave.SinesApproximation(point)
+    local wavelength = waveSettings.Calm_Wave.Wavelength
+    local amplitude = waveSettings.Calm_Wave.Amplitude
+    local speed = waveSettings.Calm_Wave.Speed
+    local windDirection = waveSettings.Calm_Wave.WindDirection
+
+    local w = 2/wavelength
+    local phaseConstant = speed * w
+
+    return amplitude * math.sin(windDirection:Dot(point) * w + clock:GetTime() * phaseConstant)
+end
+
+function wave:Update(waveName)
+    local currentWave
+    local currentConnection
+
+    for i = 1, #wave do
+        if wave[i].Name == waveName then
+            currentWave = wave[i]
+            break
+        end
+    end
+
+    for i = 1, #wave.connections do
+        if wave.connections[i].Name == waveName then
+            currentConnection = wave.connections[i]
+            break
+        end
+    end
+
+    local intantiatedConnection =  runService.Stepped:Connect(function()
+        for i,v in pairs(currentWave.bones) do
+            if v:IsA("Bone") then
+                local boneHeight = wave.SinesApproximation(Vector2.new(v.WorldPosition.X, v.WorldPosition.Z))
+                v.WorldPosition = Vector3.new(v.WorldPosition.X, boneHeight, v.WorldPosition.Z)
+            end
+        end  
+    end)
+
+     currentConnection.connection = intantiatedConnection
+end
+
+function wave:Terminate(index)
+    if index then
+        wave.connections[index].connection:Disconnect()
+    else
+        for i = 1, #wave.connections  do
+            wave.connections[i].connection:Disconnect()
+        end
+    end
+end
+
+return wave
